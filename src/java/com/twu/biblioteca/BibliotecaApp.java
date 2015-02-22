7fef914e0003ffbce1d7e9e834d277bb092204b2
fanchao-meng <fanchao.meng@rea-group.com>
2015-02-22 10:51:55 +0800

    refactor main menu

--- src/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/java/com/twu/biblioteca/BibliotecaApp.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,12 +10,20 @@ public class BibliotecaApp {
         System.out.println("0 - List Books");
 
         Scanner sc = new Scanner(System.in);
-        int i = sc.nextInt();
-        if(i==0) {
-            List<String[]> books_info = store.listBooks();
-            for(String[] book_info:books_info){
-                System.out.printf("%s %s %s\n",book_info[0],book_info[1],book_info[2]);
-            }
+        int option = sc.nextInt();
+
+        HashMap<Integer, Runnable> map = new HashMap<Integer, Runnable>();
+        Runnable[] actions = new Runnable[]{
+                new Runnable() {public void run() {listBooks();}}
+        };
+        for(int i=0;i<1;i++) map.put(i,actions[i]);
+        map.get(option).run();
+    }
+
+    private static void listBooks() {
+        List<String[]> books_info = store.listBooks();
+        for(String[] book_info:books_info){
+            System.out.printf("%s %s %s\n",book_info[0],book_info[1],book_info[2]);
         }
     }
 }
